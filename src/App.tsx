70a063067e98117c50deb32caa719b86057e971e
tanacchi <q111026d@mail.kyutech.jp>
2020-06-10 09:50:14 +0900

    Add Skills component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Access from './components/Access'
 import Affiliations from './components/Affiliations'
 import Events from './components/Events'
 import Works from './components/Works'
+import Skills from './components/Skills'
 
 function App() {
   return (
@@ -17,58 +18,7 @@ function App() {
       <Affiliations />
       <Events />
       <Works />
-      <h2 id="skills">Skills</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Skill</th>
-            <th>Level</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>C/C++</td>
-            <td>RPG ゲーム開発，ROS ノード</td>
-          </tr>
-          <tr>
-            <td>Python</td>
-            <td>Web サービス，機械学習，Lisp インタプリタ</td>
-          </tr>
-          <tr>
-            <td>ROS</td>
-            <td>自律移動ロボット開発</td>
-          </tr>
-          <tr>
-            <td>Ruby on Rails</td>
-            <td>Web サービス開発(1年間)</td>
-          </tr>
-          <tr>
-            <td>機械学習</td>
-            <td>独学3ヶ月 ＋ 大学院で研究</td>
-          </tr>
-          <tr>
-            <td>Flask</td>
-            <td>簡単なWebサービス，LINE Bot 開発</td>
-          </tr>
-          <tr>
-            <td>Ruby</td>
-            <td>Rails で使う程度</td>
-          </tr>
-          <tr>
-            <td>Lisp</td>
-            <td>趣味程度</td>
-          </tr>
-          <tr>
-            <td>Vue.js</td>
-            <td>趣味程度</td>
-          </tr>
-          <tr>
-            <td>Rust</td>
-            <td>Hello World</td>
-          </tr>
-        </tbody>
-      </table>
-
+      <Skills />
       <h2 id="internships">Internships</h2>
       <ul>
         <li>株式会社OPTiM R&Dチーム 2-weeks</li>
---
+++ src/components/Skills.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const Skills: React.FC = () => {
+  return (
+    <div id="Skills">
+      <h2>Skills</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Skill</th>
+            <th>Level</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>C/C++</td>
+            <td>RPG ゲーム開発，ROS ノード</td>
+          </tr>
+          <tr>
+            <td>Python</td>
+            <td>Web サービス，機械学習，Lisp インタプリタ</td>
+          </tr>
+          <tr>
+            <td>ROS</td>
+            <td>自律移動ロボット開発</td>
+          </tr>
+          <tr>
+            <td>Ruby on Rails</td>
+            <td>Web サービス開発(1年間)</td>
+          </tr>
+          <tr>
+            <td>機械学習</td>
+            <td>独学3ヶ月 ＋ 大学院で研究</td>
+          </tr>
+          <tr>
+            <td>Flask</td>
+            <td>簡単なWebサービス，LINE Bot 開発</td>
+          </tr>
+          <tr>
+            <td>Ruby</td>
+            <td>Rails で使う程度</td>
+          </tr>
+          <tr>
+            <td>Lisp</td>
+            <td>趣味程度</td>
+          </tr>
+          <tr>
+            <td>Vue.js</td>
+            <td>趣味程度</td>
+          </tr>
+          <tr>
+            <td>Rust</td>
+            <td>Hello World</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Skills

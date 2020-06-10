d6e2d24501f9d4a1eff6a449801ad3eeb27bbd0b
tanacchi <q111026d@mail.kyutech.jp>
2020-06-10 10:00:20 +0900

    Add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
 import Access from './components/Access'
 import Affiliations from './components/Affiliations'
 import Events from './components/Events'
@@ -11,10 +12,8 @@ import Internships from './components/Internships'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>tanacchi</h1>
-        <p>This is the tanacchi's portfolio.</p>
-      </header>
+      <Header />
+
       <Access />
       <Affiliations />
       <Events />
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header: React.FC = () => {
+  return (
+    <header className="App-header">
+      <h1>tanacchi</h1>
+      <p>This is the tanacchi's portfolio.</p>
+    </header>
+  )
+}
+
+export default Header

3ce1ba298cba3a5854f68a1757c3b5b1d5850b6e
tanacchi <q111026d@mail.kyutech.jp>
2020-06-10 09:41:55 +0900

    Add Events component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Access from './components/Access'
 import Affiliations from './components/Affiliations'
+import Events from './components/Events'
 
 function App() {
   return (
@@ -13,22 +14,7 @@ function App() {
       </header>
       <Access />
       <Affiliations />
-      <h2 id="events">Events and Hackathon</h2>
-      <ul>
-        <li>つくばチャレンジ(2016, 2017, 2018)</li>
-        <li>HACK U 2018 (Happy Hacking 賞)</li>
-        <li>enPiT-BizSysD</li>
-        <li>チャレキャラ 2018 (優秀賞)</li>
-        <li>PBL 合同成果発表会 (グランプリ)</li>
-        <li>Hack Festival Fukuoka (最優秀賞)</li>
-        <li>SPAJAM 2019</li>
-        <li>HACK U 2019</li>
-        <li>JPHACKS HackDay (Best Hack Day Award)</li>
-        <li>Hack Festival Kitakyushu (Red Bull 賞)</li>
-        <li>チャレキャラ 2019</li>
-        <li>JPHACKS AwardDay (FUJI FILM賞，Innovator認定)</li>
-      </ul>
-
+      <Events />
       <h2 id="works">Works</h2>
       <table>
         <thead>

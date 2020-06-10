000add9e78ca1d58cc2cc3ee363dba71d22a27e4
tanacchi <q111026d@mail.kyutech.jp>
2020-06-10 09:46:44 +0900

    Add Works component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import Access from './components/Access'
 import Affiliations from './components/Affiliations'
 import Events from './components/Events'
+import Works from './components/Works'
 
 function App() {
   return (
@@ -15,96 +16,7 @@ function App() {
       <Access />
       <Affiliations />
       <Events />
-      <h2 id="works">Works</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Year  </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <a
-                className="App-link"
-                href="https://github.com/tanacchi/rogue_game"
-              >
-                rogue_game
-              </a>
-            </td>
-            <td>ターミナル上で動くRPGゲーム</td>
-            <td>2018 ~</td>
-          </tr>
-          <tr>
-            <td>
-              <a
-                className="App-link"
-                href="https://github.com/jphacks/FK_1906"
-              >
-                FK_1906
-              </a>
-            </td>
-            <td>プレゼンスキルの数値化ツール</td>
-            <td>2019</td>
-          </tr>
-          <tr>
-            <td>
-              <a
-                className="App-link"
-                href="https://github.com/tanacchi/lisp_in_python"
-              >
-                lisp_in_python
-              </a>
-            </td>
-            <td>Lisp インタプリタ in Python</td>
-            <td>2019</td>
-          </tr>
-          <tr>
-            <td>
-              <a
-                className="App-link"
-                href="https://github.com/tanacchi/python_tanaline"
-              >
-                python_tanaline
-              </a>
-            </td>
-            <td>新入生教育用LINEボット</td>
-            <td>2019</td>
-          </tr>
-          <tr>
-            <td>
-              <a
-                className="App-link"
-                href="https://github.com/CIR-KIT/fifth_robot_pkg"
-              >
-                fifth_robot_pkg
-              </a>
-            </td>
-            <td>自律移動ロボット(CIR-KIT5号機)</td>
-            <td>2016 ~ 2018</td>
-          </tr>
-          <tr>
-            <td>Hello Idea</td>
-            <td>アイデア創出プラットフォーム</td>
-            <td>2018</td>
-          </tr>
-          <tr>
-            <td>
-              <a
-                className="App-link"
-                href="https://qr-official.line.me/sid/L/682dslrg.png"
-              >
-                tanakinator
-              </a>
-            </td>
-            <td>LINE Bot で動作するアキネーター</td>
-            <td>2020</td>
-          </tr>
-        </tbody>
-      </table>
-
+      <Works />
       <h2 id="skills">Skills</h2>
       <table>
         <thead>

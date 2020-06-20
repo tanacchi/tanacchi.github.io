e71574ef95a60bfafa1a519def7ad3249d4e7bb0
tanacchi <q111026d@mail.kyutech.jp>
2020-06-20 22:44:20 +0900

    Add icon on button link

--- src/components/Access.tsx
+++ src/components/Access.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
-import { Box } from '@material-ui/core'
+import { Box, SvgIcon } from '@material-ui/core'
 import Button from '@material-ui/core/Button'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import BlogIcon from '@material-ui/icons/BookOutlined'
+import TwitterIcon from '@material-ui/icons/Twitter'
+import QiitaIcon from '@material-ui/icons/Pets'
 
-type RefButtonProps = { url: string, text: string }
-const RefButton: React.FC<RefButtonProps> = ({ url, text }) => {
+type RefButtonProps = {
+  url: string,
+  text: string,
+  icon: any
+}
+const RefButton: React.FC<RefButtonProps> = ({ url, text, icon }) => {
   return (
     <Button
       href={url}
       target="_blank"
       color="primary"
-      variant="outlined">
+      variant="outlined"
+      startIcon={icon}>
       {text}
     </Button>
   )
@@ -22,6 +31,8 @@ const Access: React.FC = () => {
   const blogURL: string = "https://tanacchi.hatenablog.com/"
   const qiitaURL: string = "https://qiita.com/tanacchi/"
 
+  console.log(typeof (<GitHubIcon />))
+
   return (
     <div id="Access">
       <Box>
@@ -29,16 +40,20 @@ const Access: React.FC = () => {
         <ButtonGroup>
           <RefButton
             url={githubURL}
-            text="GitHub" />
+            text="GitHub"
+            icon={<GitHubIcon />} />
           <RefButton
             url={blogURL}
-            text="Blog" />
+            text="Blog"
+            icon={<BlogIcon />} />
           <RefButton
             url={twitterURL}
-            text="Twitter" />
+            text="Twitter"
+            icon={<TwitterIcon />} />
           <RefButton
             url={qiitaURL}
-            text="Qiita" />
+            text="Qiita"
+            icon={<QiitaIcon />} />
         </ButtonGroup>
       </Box>
     </div>

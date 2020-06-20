a8edf57a3ea17e9e3167fd8f4feaac4d9c17aa7f
tanacchi <q111026d@mail.kyutech.jp>
2020-06-20 22:45:03 +0900

    Updates

---
+++ precache-manifest.0873274722ea35d1659c4ccac5c02dc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d3a388155e4454ed8993a0b366ecf48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b9c0e8b35f3d6ff6170",
+    "url": "/static/css/main.c8ac93f4.chunk.css"
+  },
+  {
+    "revision": "023d24865d57f51fa5aa",
+    "url": "/static/js/2.22fba68d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.22fba68d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b9c0e8b35f3d6ff6170",
+    "url": "/static/js/main.4dce2085.chunk.js"
+  },
+  {
+    "revision": "d9d4ce52211fc368615e",
+    "url": "/static/js/runtime-main.5c0fe41d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.590bf1468e69c728ab194a0c498f7e2d.js"
+  "/precache-manifest.0873274722ea35d1659c4ccac5c02dc4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

777ae60857f2a0b6011a51ffa3dd61270a54d055
tanacchi <q111026d@mail.kyutech.jp>
2020-06-23 14:23:20 +0900

    Switch contents with tab (list)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 import './App.css'
 import Header from './components/Header'
 import Access from './components/Access'
@@ -9,16 +10,53 @@ import Skills from './components/Skills'
 import Internships from './components/Internships'
 
 function App() {
+  const handleSelect = (index: number, last: number) => {
+    console.log(`{last} => {index}`)
+  }
+
   return (
     <div className="App">
       <Header />
-
-      <Access />
-      <Affiliations />
-      <Events />
-      <Works />
-      <Skills />
-      <Internships />
+      <Tabs>
+        <TabList>
+          <Tab>Aceess</Tab>
+          <Tab>Affiliations</Tab>
+          <Tab>Events</Tab>
+          <Tab>Works</Tab>
+          <Tab>Skills</Tab>
+          <Tab>Internships</Tab>
+        </TabList>
+        <TabPanel>
+          <div id="tab-access">
+            <Access />
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div id="tab-affiliations">
+            <Affiliations />
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div id="tab-events">
+            <Events />
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div id="tab-works">
+            <Works />
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div id="tab-skilss">
+            <Skills />
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div id="tab-internships">
+            <Internships />
+          </div>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 }

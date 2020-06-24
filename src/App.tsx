21fae343b0a79c6782a31e9006fa40c7b11e90f8
tanacchi <q111026d@mail.kyutech.jp>
2020-06-24 20:01:43 +0900

    Revert "Switch contents with tab (list)"
    This reverts commit 777ae60857f2a0b6011a51ffa3dd61270a54d055.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 import './App.css'
 import 'react-tabs/style/react-tabs.css'
 import Header from './components/Header'
@@ -14,46 +13,13 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Tabs>
-        <TabList>
-          <Tab>Aceess</Tab>
-          <Tab>Affiliations</Tab>
-          <Tab>Events</Tab>
-          <Tab>Works</Tab>
-          <Tab>Skills</Tab>
-          <Tab>Internships</Tab>
-        </TabList>
-        <TabPanel>
-          <div id="tab-access">
-            <Access />
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <div id="tab-affiliations">
-            <Affiliations />
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <div id="tab-events">
-            <Events />
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <div id="tab-works">
-            <Works />
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <div id="tab-skilss">
-            <Skills />
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <div id="tab-internships">
-            <Internships />
-          </div>
-        </TabPanel>
-      </Tabs>
+
+      <Access />
+      <Affiliations />
+      <Events />
+      <Works />
+      <Skills />
+      <Internships />
     </div>
   );
 }

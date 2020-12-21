4c3b708bfa8f30c515c9e37b436f094bafdcefc4
tanacchi <q111026d@mail.kyutech.jp>
2020-12-21 09:28:21 +0900

    Add internship information in 2020 winter

--- src/components/Internships.tsx
+++ src/components/Internships.tsx
@@ -5,16 +5,22 @@ const Internships: React.FC = () => {
     <div id="Internships">
       <h2>Internships</h2>
       <ul>
-        <li>株式会社 OPTiM R&Dチーム 2-weeks (2019-9)</li>
+        <li>2019-9：株式会社 OPTiM R&Dチーム 2-weeks</li>
       </ul>
       <ul>
-        <li>ヤフー株式会社 Yahoo! 不動産 1-week (2020-8)</li>
+        <li>2020-8：ヤフー株式会社 Yahoo! 不動産 1-week</li>
       </ul>
       <ul>
-        <li>日本 IBM 株式会社 IT スペシャリストコース 1-week (2020-9)</li>
+        <li>2020-9：日本 IBM 株式会社 IT スペシャリストコース 1-week</li>
       </ul>
       <ul>
-        <li>株式会社 VOYAGE GROUP GrowthTech 2-days (2020-9)</li>
+        <li>2020-9：株式会社 VOYAGE GROUP GrowthTech 2-days</li>
+      </ul>
+      <ul>
+        <li>2020-12：株式会社 Speee エンジニア 1-day</li>
+      </ul>
+      <ul>
+        <li>2020-12：株式会社ワンキャリア エンジニア 2-days</li>
       </ul>
     </div>
   )
